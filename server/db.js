e0a3e99716e01b62a32fad68df835bf573341094
malankau <yyj14279@gmail.com>
2026-07-16 10:09:38 +0900

    Use Aiven DB

--- server/db.js
+++ server/db.js
@@ -1,29 +1,24 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-
-    host: "localhost",
-
-    user: "root",
-
-    password: "yyj0510",
-
-    database: "husband_test"
-
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err) => {
-
     if (err) {
-
         console.error("DB 연결 실패");
         console.error(err);
         return;
-
     }
 
     console.log("MySQL 연결 성공");
-
 });
 
 module.exports = db;
